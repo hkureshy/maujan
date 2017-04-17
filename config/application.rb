799fedb72d4a0439c016dc72682e312989f37236
Hassnain Kureshy <h.kureshy@yahoo.com>
2017-04-17 14:02:17 +0500

    asset precompile change

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'csv'
 require 'iconv'
 require 'rails/all'
 
-config.serve_static_assets = true
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -25,5 +24,6 @@ module Maujan
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end
