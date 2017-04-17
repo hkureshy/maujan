0af5295dd098c658aaab149c535097226db90004
Hassnain Kureshy <h.kureshy@yahoo.com>
2017-04-17 14:00:06 +0500

    asset precompile

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'csv'
 require 'iconv'
 require 'rails/all'
 
+config.serve_static_assets = true
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
